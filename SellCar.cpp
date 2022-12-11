92381f2ed54b9088f108e2ba02d603aee053cd30
Lmermanishvili <116061065+Lmermanishvili@users.noreply.github.com>
2022-12-11 14:24:16 +0400

    Add SellCarr file.cpp

---
+++ SellCar.cpp
@@ -0,0 +1,101 @@
+
+#include <iostream>
+
+using namespace std;
+
+// Car industry
+class CarIndustry {
+    public:
+        string CarBrend, CarModel, CarCountry;
+        
+        CarIndustry(string carBrend, string carModel, string carCountry) {
+            CarBrend = carBrend;
+            CarModel = carModel;
+            CarCountry = carCountry; 
+            cout << " Name : " << CarBrend << endl;
+            cout << " Model : " << CarModel << endl;
+            cout << " Manufacturing country : " << CarCountry << endl;
+        }
+};
+
+// Car feature
+class CarFeature {
+    public:
+        void carFeatures(int topSpeed, string color, string engine, int doors, int transmission, int price) {
+            cout << " Top speed : " << topSpeed << " km/h" << endl;
+            cout << " Color : " << color << endl;
+            cout << " Engine : " << engine << endl;
+            cout << " Doors : " << doors << endl;
+            cout << " Transmission : " << transmission << endl;
+            cout << " Price : " << price << " $" << endl;
+        }
+};
+
+// absract class
+class CarMove {
+    public:
+        virtual void move()=0;
+};
+
+// Country for sale
+class Country {
+    public:
+        void getCountry(string name) {
+            cout<< " The country where the car was sold is : " << name << endl;
+            cout<< endl;
+        }
+};
+
+class Bugatti: public CarIndustry, public CarFeature, public CarMove, public Country {
+    public:
+        Bugatti(string carBrend, string carModel, string carCountry): CarIndustry(carBrend, carModel, carCountry) {
+        
+        }
+        
+        void move() {
+            cout << " " << CarBrend << " can move" << endl;
+        }
+};
+
+class Ferrari: public Bugatti {
+    public:
+        Ferrari(string carBrend, string carModel, string carCountry): Bugatti(carBrend, carModel, carCountry) {
+        
+        }
+        void move() {
+            cout << " " << CarBrend << " can move" << endl;
+        }
+};
+class Lamborghini: public Bugatti {
+    public:
+        Lamborghini(string carBrend, string carModel, string carCountry): Bugatti(carBrend, carModel, carCountry) {
+        
+        }
+        void move() {
+            cout << " " << CarBrend << " can move" << endl;
+        }
+};
+    
+
+
+int main()
+{ 
+
+Bugatti bugatti("Bugatti", "Veiron", "France");
+bugatti.carFeatures(431, "Black", "8.0 L", 2, 7, 1599950);
+bugatti.move();
+bugatti.getCountry("United Arab Emirates");
+
+Ferrari ferrari("Ferrari", "F8 spider", "Itally");
+ferrari.carFeatures(340, "Red", "3.9 L", 2, 7, 274280);
+ferrari.move();
+ferrari.getCountry("USA States");
+
+Lamborghini lamborghini("Lamborghini", "Murciélago LP 670 –4 SuperVeloce", "Itally");
+lamborghini.carFeatures(342, "Orange", "6.5 L", 2, 7, 450000);
+lamborghini.move();
+lamborghini.getCountry("United Arab Emirates");
+
+
+    return 0;
+}
